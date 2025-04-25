85e2c1c0bd3862874e3f561d00341a179756656a
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2025-04-24 21:47:57 -0300

    chore: folder structure

--- src/database.ts
+++ src/database.ts
@@ -1,16 +1,14 @@
 import { type Knex, knex as setupKnex } from "knex";
 
-export const config: { [key: string]: Knex.Config } = {
-	development: {
-		client: "sqlite3",
-		connection: {
-			filename: "./tmp/app.db",
-		},
-		migrations: {
-			directory: "./database/migrations",
-		},
-		useNullAsDefault: true,
+export const config: Knex.Config = {
+	client: "sqlite3",
+	connection: {
+		filename: "./database/app.db",
 	},
+	migrations: {
+		directory: "./database/migrations",
+	},
+	useNullAsDefault: true,
 };
 
-export const knex = setupKnex(config.development);
+export const knex = setupKnex(config);
