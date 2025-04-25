944a16d576f18496589d3f5f6818b0532b91524a
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2025-04-25 16:08:20 -0300

    feat: test database

--- test/transactions.test.ts
+++ test/transactions.test.ts
@@ -1,11 +1,17 @@
+import { execSync } from "node:child_process";
 import { describe } from "node:test";
 import request from "supertest";
-import { afterAll, beforeAll, expect, test } from "vitest";
+import { afterAll, beforeAll, beforeEach, expect, test } from "vitest";
 import { app } from "../src/app";
 
 describe("Transactions routes", () => {
 	beforeAll(async () => {
 		await app.ready();
+	});
+
+	beforeEach(() => {
+		execSync("npm run knex migrate:rollback --all");
+		execSync("npm run knex migrate:latest");
 	});
 
 	afterAll(async () => {
@@ -20,5 +26,31 @@ describe("Transactions routes", () => {
 		});
 
 		expect(response.statusCode).toBe(201);
+	});
+
+	test("user can list all transactions", async () => {
+		const createTransactionResponse = await request(app.server)
+			.post("/transactions")
+			.send({
+				title: "New transaction",
+				amount: 5000,
+				type: "credit",
+			});
+
+		const cookies = createTransactionResponse.get("Set-Cookie");
+
+		if (!cookies) throw new Error("no cookies were found");
+
+		const listTransactionsResponse = await request(app.server)
+			.get("/transactions")
+			.set("Cookie", cookies)
+			.expect(200);
+
+		expect(listTransactionsResponse.body.transactions).toEqual([
+			expect.objectContaining({
+				title: "New transaction",
+				amount: 5000,
+			}),
+		]);
 	});
 });
